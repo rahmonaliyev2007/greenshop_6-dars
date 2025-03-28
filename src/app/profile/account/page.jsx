715c1250538e6a554b6d5c70a06267117e05a38c
Rahmonaliyev Abdulaziz <142968268+RahmonaliyevAbdulaziz@users.noreply.github.com>
2025-03-28 10:15:40 +0500

    profile css fixed

--- src/app/profile/account/page.jsx
+++ src/app/profile/account/page.jsx
@@ -11,35 +11,35 @@ export default function Account() {
         <form>
             <div className='flex gap-3 justify-between items-center'>
             <label className='w-full my-3' >
-                <div> <span className='text-red-500'>*</span> Fist Name</div>
+                <div className='font-semibold text-sm'> <span className='text-red-500 '>*</span> Fist Name</div>
                 <input className='w-full my-2 py-2 px-3 rounded-lg border bg-white' type="text" value={user.name}/>
             </label>
             <label className='w-full my-3' >
-                <div> <span className='text-red-500'>*</span> Last Name</div>
+                <div className='font-semibold text-sm'> <span className='text-red-500 '>*</span> Last Name</div>
                 <input className='w-full my-2 py-2 px-3 rounded-lg border bg-white' type="text" value={user.surname}/>
             </label>
             </div>
             <div className='flex gap-3 justify-between items-center'>
             <label className='w-full my-3' >
-                <div> <span className='text-red-500'>*</span> Email Address</div>
+                <div className='font-semibold text-sm'> <span className='text-red-500 '>*</span> Email Address</div>
                 <input className='w-full my-2 py-2 px-3 rounded-lg border bg-white' type="email" value={user.email}/>
             </label>
             <label className='w-full my-3' >
-                <div> <span className='text-red-500'>*</span> Phone Number</div>
+                <div className='font-semibold text-sm'> <span className='text-red-500 '>*</span> Phone Number</div>
                 <input className='w-full my-2 py-2 px-3 rounded-lg border bg-white' type="text" value={user.phone_number}/>
             </label>
             </div>
             <div className='flex gap-3 justify-between items-center'>
             <label className='w-full my-3' >
-                <div> <span className='text-red-500'>*</span> Username</div>
+                <div className='font-semibold text-sm'> <span className='text-red-500 '>*</span> Username</div>
                 <input className='w-full my-2 py-2 px-3 rounded-lg border bg-white' type="text" value={user.username}/>
             </label>
             <label className='w-full my-3' >
-                <div> <span className='text-red-500'></span> Photo</div>
+                <div className='font-semibold text-sm'> <span className='text-red-500 '></span> Photo</div>
                 <input className='w-1/2  my-2block py-2 px-3 rounded-lg border bg-white' type="file" value={user.username}/>
             </label>
             </div>
-            <button className='bg-[#46A358] text-white py-2 px-3 cursor-pointer rounded font-semibold'>Save Changes</button>
+            <button className='bg-[#46A358] hover:bg-[#46A358]/80 text-white py-2 px-3 cursor-pointer rounded font-semibold'>Save Changes</button>
         </form>
     </div>
   )
